c367163419d3405e516a9aa338aae8f7ab881c8d
motthy0 <sanscoding0@gmail.com>
2023-08-14 20:23:04 +0700

    controls for dark mode

--- script.js
+++ script.js
@@ -1,49 +1,15 @@
-window.onload = function(){
-    var seconds = 0;
-    var tens = 0;
-    var OutputSeconds = document.getElementById("second");
-    var OutputTens = document.getElementById("tens");
-    var buttonStart = document.getElementById("btn-start");
-    var buttonStop = document.getElementById("btn-stop");
-    var buttonReset = document.getElementById("btn-reset");
-    var Interval;
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
 
-    buttonStart.addEventListener('click', () => {
-        clearInterval(Interval);
-        Interval = setInterval(startTimer, 10);  // millisecond 10 = 0.01 second
-    });
-
-    buttonStop.addEventListener('click', () => {
-        clearInterval(Interval);
-    });
-
-    buttonReset.addEventListener('click', () => {
-        clearInterval(Interval);
-        tens = "00";
-        seconds = "00";
-        OutputSeconds.innerHTML = seconds;
-        OutputTens.innerHTML = tens;
-    });
-
-    function startTimer(){
-        tens++;
-        if(tens <= 9){
-            OutputTens.innerHTML = "0" + tens;
-        }
-
-        if(tens > 9){
-            OutputTens.innerHTML = tens;
-        }
-
-        if(tens > 99){
-            seconds++;
-            OutputSeconds.innerHTML = "0" + seconds;
-            tens = 0;
-            OutputTens.innerHTML = "0" + 0;
-        }
-
-        if(seconds > 9){
-            OutputSeconds.innerHTML = seconds;
-        }
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
     }
-}
+});
