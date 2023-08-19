fe9b31a9b8dfa3ffc43620fc84ce70ea0e353fb9
motthy0 <sanscoding0@gmail.com>
2023-08-19 23:02:11 +0700

    control of bg random color

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
-let W = window.innerWidth;
-let H = window.innerHeight;
+const button = document.getElementById('btn');
+const body = document.querySelector('body');
+const color = ['red', 'green', 'blue', 'yellow', 'pink', 'grey', 'lime'];
 
-canvas.width = W;
-canvas.height = H;
-
-let fontSize = 16;
-let columns = Math.floor(W / fontSize);
-let drops = [];
-for(let i=0; i<columns; i++){
-    drops.push(0);
-}
-let str = "JavaScript Hacking Effect";
-export default function draw(){
-    context.fillStyle = "rgba(0,0,0,0.05)";
-    context.fillRect(0, 0, W, H);
-    context.fontSize = "700 " + fontSize + "px";
-    context.fillStyle = "#00cc33";
-    for(let i=0; i<columns; i++){
-        let index = Math.floor(Math.random()*str.length);
-        let x = i * fontSize;
-        let y = drops[i] * fontSize;
-        context.fillText(str[index], x, y);
-        if(y >= canvas.height && Math.random() > 0.99){
-            drops[i] = 0;
-        }
-        drops[i]++;
-    }
-}
-draw();
-setInterval(draw, 35);
+body.style.backgroundColor = 'black';
+button.addEventListener('click', () => {
+    const colorIndex = parseInt(Math.random() * color.length);
+    body.style.backgroundColor = color[colorIndex];
+    body.style.transition = '1s linear';
+});
